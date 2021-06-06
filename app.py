653f478735085c2b45c93e9b5a9c734a1a67c5d8
GitSloth <niemands110@gmail.com>
2021-06-06 17:49:51 +0200

    added something to send data to

--- app.py
+++ app.py
@@ -6,18 +6,27 @@ app = Flask(__name__)
 
 ledFlag = 0 
 
+#unused POST method
 @app.route('/postjson', methods = ['POST'])
 def postJsonHandler():
     content = request.get_json()
     print(content)
     return 'JSON posted'
     
-    
+#testrun  
 @app.route('/testrun')
 def testrunner():
     return 'please receive this'
 
 
-@app.rout('/ledFlag')
+@app.rout('/ledflag')
 def giveLEDFlag():
     return ledFlag
+
+@app.route('/getinfo', methods = ['POST','GET'])
+def receiving():
+    data = float(request.args.get('irdata'))
+    if irdata > 10:
+        ledFlag = 0
+    else:
+        ledFlag = 1
