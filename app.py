db7dc0712c83c491e960991f3caded4a860c67aa
GitSloth <niemands110@gmail.com>
2021-06-06 18:12:21 +0200

    help me please

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 
 ledFlag = 0 
 
-#unused POST method
+
 @app.route('/postjson', methods = ['POST'])
 def postJsonHandler():
     content = request.get_json()
     print(content)
     return 'JSON posted'
-    
-#testrun  
+
 @app.route('/testrun')
 def testrunner():
     return 'please receive this'
